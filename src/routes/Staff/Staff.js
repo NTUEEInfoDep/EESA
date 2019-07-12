a3f8b9e670b660909a6d48bff97f43c6ed1da6ba
Brian Chao <whereisbrianchao520@gmail.com>
2019-07-12 20:10:26 +0900

    add staff template

--- src/routes/Staff/Staff.js
+++ src/routes/Staff/Staff.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import css from './Staff.module.css';
+import FaceIcon from '@material-ui/icons/FaceOutlined';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    icon: {
+        fontSize: "5vh",
+        marginRight: "2%",
+    },
+})
 
 class Staff extends React.Component{
 
     render(){
         const { id } = this.props.match.params;
+        const { classes } = this.props;
+
         return(
+
             <div className={css.page}>
-                Staffs {id}.
+                <div className={css.title}><FaceIcon className={classes.icon}/>{id}</div>
+                <div className={css.lowerPage}></div>
             </div>
         )
     }
 }
 
-export default Staff
+export default withStyles(styles)(Staff);
